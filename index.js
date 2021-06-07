40067aa0cffacf384a6a53d293f1d68fd35b0b8f
Marc Steele <marcsteele2014@gmail.com>
2021-06-07 19:40:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findMinAndRemove(array){
-
+  let min = array[0]
+  let minIndex = 0
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i]
+      minIndex = i
+    }
+  }
+  array.splice(minIndex,1)
+  return min
 }
 
 function selectionSort(array){
-
+  let sorted = []
+  let min;
+  while(array.length != 0){
+    min = findMinAndRemove(array)
+    sorted.push(min)
+  }
+  return sorted;
 }
